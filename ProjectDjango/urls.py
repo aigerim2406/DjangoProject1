d596c85398d9ab42c6135499383bf899c663d6f5
aigerim2406 <aigerim.gubaidullina@narxoz.kz>
2023-04-08 16:41:49 +0600

    baozh 2

--- ProjectDjango/urls.py
+++ ProjectDjango/urls.py
@@ -13,14 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from aigerim.views import *
 from django.urls import path, include
 
+from ProjectDjango import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('aigerim.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
--- aigerim/models.py
+++ aigerim/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Aigerim(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    is_published = models.BooleanField(default=True)
