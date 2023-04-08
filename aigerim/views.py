6810c2a0894d8d32993929f733915cfd667e11cc
aigerim2406 <aigerim.gubaidullina@narxoz.kz>
2023-04-08 15:51:04 +0600

    baozh 1

--- aigerim/views.py
+++ aigerim/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):  # HttpRequest
+    return HttpResponse("Страница приложения women.")
+
+
+def categories(request, catid):
+    if request.POST:
+        print(request.POST)
+    return HttpResponse("<h1>Статьи по категориям</h1><p>{catid}</p>")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
