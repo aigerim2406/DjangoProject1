57dfdc5ea2aaa70d155a0d6097ac4a5b9559d7cf
aigerim2406 <aigerim.gubaidullina@narxoz.kz>
2023-04-09 13:54:39 +0600

    baozh 6

--- aigerim/views.py
+++ aigerim/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+
 
 from .forms import *
 from .models import *
@@ -9,35 +12,64 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         {'title': "Войти", 'url_name': 'login'}
 ]
-def index(request):
-    posts = Aigerim.objects.all()
-    cats = Category.objects.all()
-    context = {
-        'posts': posts,
-        'cats': cats,
-        'menu': menu,
-        'title': 'Главная страница',
-        'cat_selected': 0,
-    }
-    return render(request, 'aigerim/index.html', context=context)
+
+class AigerimHome(ListView):
+    model = Aigerim
+    template_name = 'aigerim/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Aigerim.objects.filter(is_published=True)
+
+class AigerimCategory(ListView):
+    model = Aigerim
+    template_name = 'aigerim/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Aigerim.objects.filter(cat__slug=self.kwargs['cat_slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['menu'] = menu
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
+
+class ShowPost(DetailView):
+    model = Aigerim
+    template_name = 'aigerim/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        context['menu'] = menu
+        return context
+
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'aigerim/addpage.html'
+    success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+
+    def get_context_data(self,*, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавить'
+        context['menu'] = menu
+        return context
 
 def about(request):
     return render(request, 'aigerim/about.html', {'menu': menu, 'title': 'О сайте'})
-
-
-def addpage(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST)
-        if form.is_valid():
-            # print(form.cleaned_data)
-            try:
-                Aigerim.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавление сайта')
-    else:
-        form = AddPostForm()
-    return render(request, 'aigerim/addpage.html', {'form': form,'menu': menu, 'title': "Добавить"})
 
 def contact(request):
     return HttpResponse("Обратная связь")
@@ -48,32 +80,57 @@ def login(request):
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Aigerim, slug=post_slug)
 
-    context = {
-        'post': post,
-        'menu': menu,
-        'title': post.title,
-        'cat_selected': post.cat_id,
-    }
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'aigerim/addpage.html', {'form': form, 'menu': menu, 'title': "Добавить"})
 
-    return render(request,'aigerim/post.html', context=context)
 
-def show_category(request, cat_id):
-    posts = Aigerim.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Aigerim, slug=post_slug)
+#
+#     context = {
+#         'post': post,
+#         'menu': menu,
+#         'title': post.title,
+#         'cat_selected': post.cat_id,
+#     }
+#
+#     return render(request,'aigerim/post.html', context=context)
 
-    if len(posts) == 0:
-        raise Http404()
+# def show_category(request, cat_id):
+#     posts = Aigerim.objects.filter(cat_id=cat_id)
+#     cats = Category.objects.all()
+#
+#     if len(posts) == 0:
+#         raise Http404()
+#
+#     context = {
+#         'posts': posts,
+#         'cats': cats,
+#         'menu': menu,
+#         'title': 'Отображение по рубрикам',
+#         'cat_seletced': cat_id,
+#     }
+#
+#     return render(request, 'aigerim/index.html', context=context)
 
-    context = {
-        'posts': posts,
-        'cats': cats,
-        'menu': menu,
-        'title': 'Отображение по рубрикам',
-        'cat_seletced': cat_id,
-    }
 
-    return render(request, 'aigerim/index.html', context=context)
-
+# def index(request):
+#     posts = Aigerim.objects.all()
+#     cats = Category.objects.all()
+#     context = {
+#         'posts': posts,
+#         'cats': cats,
+#         'menu': menu,
+#         'title': 'Главная страница',
+#         'cat_selected': 0,
+#     }
+#     return render(request, 'aigerim/index.html', context=context)
